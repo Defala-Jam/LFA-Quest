ad0f6b56b0dd312068f45645f54a6f46d9ace913
Defala <defalaemprego@gmail.com>
2025-10-08 22:51:11 -0400

    add login + cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Perfil from './pages/Perfil/Perfil.tsx';
 import Statistics from './pages/Statistics/Statistics.tsx';
 import Store from './pages/Store/Store.tsx'; 
 import Index from './pages/Index/Index.tsx';
+import Login from './pages/Login/login.tsx';
+import Signup from './pages/Signup/Signup.tsx';
 import "./Global.css"
 
 
@@ -13,7 +15,9 @@ function App() {
     <Router>
       <Routes>
 
-        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Index />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/path" element={<Path_player />} />
         <Route path='/profile' element={<Perfil/>} />
         <Route path="/leaderboard" element={<Leaderboard />} />
